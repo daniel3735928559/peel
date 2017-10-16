ed711f69f567894770d0f0096d3fda3755d67b2c
DR <daniel3735928559@gmail.com>
2017-10-16 01:54:51 +0000

    joke capability

--- otto/otto.py
+++ otto/otto.py
@@ -1,12 +1,34 @@
 from libmango import *
+import random
 
 class otto(m_node):
     def __init__(self):
         super().__init__(debug=True)
         self.interface.add_interface('otto.yaml',{'im_recv':self.im_recv})
-        self.debug_print("running")
+        self.help_text = self.read_whole_file("./help")
+        self.jokes = []
+        f = open("./jokes","r")
+        for l in f:
+            self.jokes += [l[:-1].split("...")]
         self.run()
+        
+    def read_whole_file(self,fn):
+        with open(fn) as f: return f.read()
+
     def im_recv(self,header,args):
-        return "im_send_to",{"msg":"otto here","conv":args['conv']}
+        cmd = args['message']
+        if(cmd == "!joke"):
+            j = random.choice(self.jokes)
+            self.debug_print("JOKE",j)
+            i = 0
+            l = len(j)
+            for line in j:
+                self.m_send("im_send_to",{"msg":"[otto.joke]: " + line,"conv":args['conv']})
+                i += 1
+                if i < l and l > 1:
+                    time.sleep(2)
+
+        else:
+            self.m_send("im_send_to",{"msg":"[otto.help]: " + self.help_text,"conv":args['conv']})
     
 otto()
