9755608b78057f9f0d92c8d791849da2c8767308
DR <daniel3735928559@gmail.com>
2017-10-20 01:20:36 +0000

    emacs insert and buffer working

--- emacs/emacs.py
+++ emacs/emacs.py
@@ -7,24 +7,52 @@ class emacs_agent(m_node):
             self.pid = os.getpid()
             self.emacs_bind = "tcp://*:55521"
 
-            self.interface.add_interface('emacs.yaml',{'insert':self.insert})
+            self.interface.add_interface('emacs.yaml',{'insert':self.insert,
+                                                       'buffer':self.get_buffer})
 
             self.emacs_socket = self.context.socket(zmq.STREAM)
             self.emacs_socket.bind(self.emacs_bind)
             self.add_socket(self.emacs_socket, self.emacs_input, self.emacs_error)
             self.sender = None
+            self.recv_buffer = ""
             self.run()
 
       def insert(self,header,args):
             if self.sender is None:
                   self.debug_print("NO SENDER")
                   return
-            self.debug_print("SENDING",args['str'],"TO",self.sender)
+            self.emacs_send("insert",{"text":args['str']})
+
+      
+      def get_buffer(self,header,args):
+            if self.sender is None:
+                  self.debug_print("NO SENDER")
+                  return
+            self.emacs_send("buffer",{})
+            
+      def emacs_send(self, name, args):
+            args['_name'] = name
+            data = json.dumps(args)
+            msg = str(len(data)) + "\n" + data
+            self.debug_print("SENDING",msg,"TO",self.sender)
             self.emacs_socket.send(self.sender,flags=zmq.SNDMORE)
-            self.emacs_socket.send_string(args['str'],flags=zmq.SNDMORE)
+            self.emacs_socket.send_string(msg,flags=zmq.SNDMORE)
             
       def emacs_error(self,msg):
             self.debug_print("EMACSSHOCK DX")
+
+      def check_buffer(self):
+            try:
+                  l,d = self.recv_buffer.split("\n",1)
+                  l = int(l)
+                  if l > len(d):
+                        return None
+                  print("SPLITTING",d[:l],d[l:])
+                  self.recv_buffer = d[l:]
+                  return d[:l]
+            except:
+                  traceback.print_exc()
+                  return None
             
       def emacs_input(self):
             sender = self.emacs_socket.recv()
@@ -32,7 +60,15 @@ class emacs_agent(m_node):
                   self.m_send("message",{"str":"Connected"})
             self.sender = sender
             msg = self.emacs_socket.recv().decode('utf-8')
-            if len(msg) > 0: 
-                  self.m_send("message",{"str":msg})
+            if len(msg) == 0: return
+            self.recv_buffer += msg
+            print("GOT",msg)
+            
+            data = self.check_buffer()
+            while not data is None:
+                  print("DATA",data)
+                  self.m_send("message",{"str":data})
+                  data = self.check_buffer()
+            print("done")
 
 emacs_agent()
--- emacs/mango.el
+++ emacs/mango.el
@@ -4,18 +4,28 @@
 
 (defvar *mango-buffer* "")
 
-(defun mango-init (host port) (make-network-process :name "mangoclient" :buffer "*mango*" :host host :family 'ipv4 :service port :sentinel 'mango-err :filter 'mango-recv))
+(defvar *mango-host* "")
+(defvar *mango-port* "")
+
+(defun mango-init (host port)
+  (setq *mango-buffer* "")
+  (setq *mango-host* host)
+  (setq *mango-port* port)
+  (make-network-process :name "mangoclient" :buffer "*mango*" :host host :family 'ipv4 :service port :sentinel 'mango-err :filter 'mango-recv))
   
 (defun mango-check-complete (input) 
   (let* ((match-index (string-match-p "\n" input))
-	 (data-len (number-to-string (substring input 0 match-index)))
-	 (data (substring input (+ 1 match-index)))
-	 (current-len (if match-index (length data) -1)))
-    (if (>= data-len current-len)
+	 (data-len (string-to-number (substring input 0 match-index)))
+	 (data-val (substring input (+ 1 match-index)))
+	 (current-len (if match-index (length data-val) -1)))
+    (if (>= current-len data-len)
 	(progn
-	  (setq *mango-buffer* (substring input (+ match-index data-len)))
-	  (substring input match-index (+ match-index data-len)))
-      nil)))
+	  (message (concat "M1 " *mango-buffer*))
+	  (setq *mango-buffer* (substring input (+ match-index data-len 1)))
+	  (message (concat "M2 " *mango-buffer*))
+	  (message (concat "M4 " (substring input (+ 1 match-index) (+ match-index data-len 1))))
+	  (substring input (+ 1 match-index) (+ match-index data-len 1)))
+      (message (concat "M3 " *mango-buffer* " " (number-to-string data-len) ":" (number-to-string current-len))) nil)))
 
 (defun mango-handle (data)
   (let* ((args (json-read-from-string data))
@@ -25,26 +35,33 @@
 
 (defun mango-recv (process event)
   (setq *mango-buffer* (concat *mango-buffer* event))
-  (let ((data (mango-check-complete *mango-buffer*)))
-    (when data (mango-handle data))
+  (let* ((data (mango-check-complete *mango-buffer*)))
+    (when data (mango-handle data))))
   
-(defun mango-err (&rest args) (message "Disconnected. Reconnceting...") (sit-for 3) (mango-init))
+;; (defun mango-err (&rest args)
+;;   (message "Disconnected. Reconnceting...")
+;;   (sit-for 3)
+;;   (mango-init *mango-host* *mango-port*))
+(defun mango-err (&rest args) (message "Disconnected."))
 
 (defun mango-stop () (delete-process "mangoclient"))
 
 (defun mango-send (name args)
-  (let* ((name-args (cons '('_name . name) args)
-	 (data (json-encode name-args))
-	 (len (length msg))
+  (let* ((name-args (cons `(_name . ,name) args))
+	 (data (json-encode-alist name-args))
+	 (len (length data))
+	 (x (message (concat "SENDING " data)))
 	 (msg (concat (number-to-string len) "\n" data)))
-    (process-send-string "mangoclient" msg))))
+    (process-send-string "mangoclient" msg)))
 
 (defun mx () (process-send-string "mangoclient" (read-string "mx: ")))
 
 (defun mango-insert (args) (insert (alist-get 'text args)))
 
-(defun mango-buffer (args) (mango-send ("buffer" (text . (let ((s (buffer-string))) (set-text-properties 0 (length s) nil s) (message s))))))
-
-(mango-init "127.0.0.1" 55521)
+(defun mango-buffer (args) (mango-send "buffer" (let ((s (buffer-string))) (set-text-properties 0 (length s) nil s) (list `(text . ,s)))))
 
 (mx)
+
+(mango-stop)
+
+(mango-init "127.0.0.1" 55521)
